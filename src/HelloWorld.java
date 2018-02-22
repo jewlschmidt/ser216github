70eed555e66e84f39a6585112cbb4b564c772f33
OnlyTanner <lisonbeetanner@gmail.com>
2018-02-22 14:12:14 -0700

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,10 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello World");
+		if ("Hello World".equals("Hello World"))
+			System.out.println("Hello World");
+		else
+			System.out.println("What...");
 	}
 
 }
